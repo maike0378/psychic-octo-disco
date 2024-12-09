00522513031bb81d8fd2dc1f4509b822d3b015a2
maike0378 <grabisch@mediamarktsaturn.com>
2024-12-09 11:10:16 +0100

    Update CSVM-5440.js

--- CSVM-5440.js
+++ CSVM-5440.js
@@ -6,5 +6,5 @@ fetch('https://www.mediamarkt.hu/webapp/wcs/stores/servlet/MultiChannelMAMasterD
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   },
-  body: `storeId=19801&langId=-19&addressType=billing&URL=https%3A%2F%2Fwww.mediamarkt.hu%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FMultiChannelMAMasterDataUpdate&demographicField4=0&isB2BAddress=false&authToken=pofuigovjom6indf7b85o6d3s4&personTitle=Mr&lastName=DOUM&firstName=DOUM&displayName=DOUM+D.&businessTitle=Andr%C3%A1ssy+%C3%BA&zipCode=1062&city=+Budapest&address1=+rue+des+doumbe&address3=22&streetType=k%C3%B6r%C3%BAt&address2=22+rue+des+doumbe&country=Franciaorsz%C3%A1g&phone2=%2B33782830011&phone1=%2B36782830897&birthdayYear=1999&birthdayMonth=10&birthdayDay=16&email1=${email}`
+  body: `storeId=19801&langId=-19&addressType=billing&URL=https%3A%2F%2Fwww.mediamarkt.hu%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FMultiChannelMAMasterDataUpdate&demographicField4=0&isB2BAddress=false&authToken=pofuigovjom6indf7b85o6d3s4&personTitle=Mr&lastName=DOUM&firstName=DOUM&displayName=DOUM+D.&businessTitle=test%C3%A1ssy+%C3%BA&zipCode=1062&city=+Budapest&address1=+rue+des+doumbe&address3=22&streetType=k%C3%B6r%C3%BAt&address2=22+rue+des+doumbe&country=Franciaorsz%C3%A1g&phone2=%2B33782830011&phone1=%2B36782830897&birthdayYear=1999&birthdayMonth=10&birthdayDay=16&email1=${email}`
 });
